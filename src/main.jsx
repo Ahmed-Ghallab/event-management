36e3aec48f0d4e9093cdf4ddb8716874349c4a6c
Ahmed Mohamed Ghallab <ahghallab1@gmail.com>
2025-07-30 22:18:37 +0300

    Fix: corrected app.css path and build issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './app.css';
+import './App.css';
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
